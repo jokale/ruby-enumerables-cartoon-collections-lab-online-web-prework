11d0644131f7ab928ea653b23c381b9358c93b84
Joanna E K <kalemajoanna@gmail.com>
2019-11-10 22:01:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,10 +22,8 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.any? do|cheese_types| return cheese_types
-  # if array.include?(cheese_types)
-  #   return "#{cheese_types}"
-  # end
-else 
-  nil 
-end 
+  # if array.any? do|cheese_types| return cheese_types
+  # # if array.include?(cheese_types)
+  # #   return "#{cheese_types}"
+  # # end
+
