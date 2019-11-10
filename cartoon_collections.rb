3ec64ff4f42761c0a138bf39eedf2e0f69ddbc95
Joanna E K <kalemajoanna@gmail.com>
2019-11-10 19:30:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
    
-["Dopey", "Grumpy", "Bashful"].each_with_index do |array, index| p  
+["Dopey", "Grumpy", "Bashful"].each_with_index do |array, index| p index 
 end 
 end
 
