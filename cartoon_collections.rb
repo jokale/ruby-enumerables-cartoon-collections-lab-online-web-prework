ca5ca43316d03ca27dd540f6f58f28eca5a90429
Joanna E K <kalemajoanna@gmail.com>
2019-11-10 20:54:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 
 def summon_captain_planet(array)
-  ["earth", "wind", "fire", "water", "heart"].map do  |array | p "#{array.capitalize}!"
+  ["carrot", "cucumber", "pepper"].map do  |array | p "#{array.capitalize}!"
 end
 end 
 
