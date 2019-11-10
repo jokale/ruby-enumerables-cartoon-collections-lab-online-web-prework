bac79d0a65f36107c025e2d7ac01b0665b497c28
Joanna E K <kalemajoanna@gmail.com>
2019-11-10 21:39:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
+  if array.include?(cheddar)
     p cheese_types
   end 
      nil 
